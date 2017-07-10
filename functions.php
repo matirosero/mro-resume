a9176daf82ce393e4e009cdca73dc1cf79bce583
MatiRosero <gekidasa@gmail.com>
2017-07-09 18:37:22 -0600

    moved main enqueue to includes

--- functions.php
+++ functions.php
@@ -3,16 +3,8 @@
 // Enqueue and dequeue
 include( 'includes/enqueue.php' );
 
-add_action( 'wp_enqueue_scripts', 'resume_parent_theme_enqueue_styles' );
 
-function resume_parent_theme_enqueue_styles() {
-    // wp_enqueue_style( 'resume-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'mro-resume-style',
-        get_stylesheet_directory_uri() . '/assets/dist/css/style.css'
-    );
-
-}
-
+// Debug functions
 include( 'includes/debug.php' );
 
 
--- includes/enqueue.php
+++ includes/enqueue.php
@@ -55,3 +55,12 @@ function mro_deregister_scripts() {
 add_action('wp_print_styles', 'mro_deregister_scripts', 99999);
 
 
+add_action( 'wp_enqueue_scripts', 'resume_parent_theme_enqueue_styles' );
+
+function resume_parent_theme_enqueue_styles() {
+    // wp_enqueue_style( 'resume-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'mro-resume-style',
+        get_stylesheet_directory_uri() . '/assets/dist/css/style.css'
+    );
+
+}
